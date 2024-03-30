779d13986ff9c9ae5a0dbba4ba69fc3222a6a989
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-03-30 22:48:23 +0500

    Create module crud

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
+import { ModelModule } from './modules/model/model.module';
+import { CompanyModule } from './modules/company/company.module';
+import { FileModule } from './modules/file/file.module';
+import { CarModule } from './modules/car/car.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+@Module({
+  imports: [AuthModule, UserModule, CarModule, FileModule, CompanyModule, ModelModule, TransactionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
